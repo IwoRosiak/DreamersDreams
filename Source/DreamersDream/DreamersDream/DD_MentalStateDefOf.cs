3a475cc4d7f5aa7b48aecd402134f56213c5bb0b
QarsoonMeel <qarsoonmeel@gmail.com>
2021-06-16 11:14:02 +0100

    Sleepwalking is fully implemented.

--- Source/DreamersDream/DreamersDream/DD_MentalStateDefOf.cs
+++ Source/DreamersDream/DreamersDream/DD_MentalStateDefOf.cs
@@ -16,9 +16,13 @@ namespace DreamersDream
 
         public static DD_MentalStateDef SleepwalkBerserk;
 
+        public static DD_MentalStateDef SleepwalkSafe;
+
         public static DD_MentalStateDef SleepwalkTantrum;
 
-        public static DD_MentalStateDef SleepwalkFireStarter;
+        //public static DD_MentalStateDef SleepwalkFireStarter;
+
+        public static DD_MentalStateDef SleepwalkOwnRoom;
 
         // Token: 0x04003A64 RID: 14948
         // public static MentalStateDef Binging_DrugExtreme;
